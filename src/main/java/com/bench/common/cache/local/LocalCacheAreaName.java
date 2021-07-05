4729f4baa5d8720be72030844702cba0432deda3
DESKTOP-J3AVJEG\admin <woshidazhu689@163.com>
2021-07-06 01:18:59 +0800

    新增一些模型

---
+++ src/main/java/com/bench/common/cache/local/LocalCacheAreaName.java
@@ -0,0 +1,11 @@
+package com.bench.common.cache.local;
+
+import com.bench.common.cache.CacheAreaName;
+
+/**
+ * @className LocalCacheAreaName
+ * @autor cold
+ * @DATE 2021/7/6 1:18
+ **/
+public interface LocalCacheAreaName extends CacheAreaName {
+}
