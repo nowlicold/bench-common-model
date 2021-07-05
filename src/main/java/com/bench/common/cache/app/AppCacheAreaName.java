f8e9d59d9914e03f55e4865e8040e30349871899
DESKTOP-J3AVJEG\admin <woshidazhu689@163.com>
2021-07-06 00:53:46 +0800

    调整了 模型，将appCacheAreaName 转义到MODEL中

---
+++ src/main/java/com/bench/common/cache/app/AppCacheAreaName.java
@@ -0,0 +1,15 @@
+package com.bench.common.cache.app;
+
+/**
+ * @className AppCacheAreName
+ * @autor cold
+ * @DATE 2021/6/21 20:02
+ **/
+public interface AppCacheAreaName {
+    /**
+     * 返回区域名
+     *
+     * @return
+     */
+    public String name();
+}
